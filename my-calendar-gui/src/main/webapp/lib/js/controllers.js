d05e8f0a160debede04b8f3f2f2859fe3cad2426
Tonté Pouncil <pouncilt.developer@gmail.com>
2012-03-10 16:59:59 -0600

    Open - my-calendar-gui mcg1: Ensure the calendar's month view displays the correct date on the correct day. https://github.com/pouncilt/my-calendar-gui/issues/1
    Working on making the month calendar view dynamic based on the current
    month.

---
+++ my-calendar-gui/src/main/webapp/lib/js/controllers.js
@@ -0,0 +1,6 @@
+/* App Controllers */
+
+function CalendarCtrl() {
+	this.weeks = createStaticWeekForFebruary();
+	//this.weeks = [{"sunday": "29", "monday": "30", "tuesday": "31", "wednesday": "1", "thursday": "2", "friday": "3", "saturday":"4"}];
+}

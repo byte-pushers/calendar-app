4fb8209af3851bcbbec04eb7c3ab98d405742e2c
Tonté Pouncil <pouncilt.developer@gmail.com>
2012-04-21 20:02:55 -0500

    Converted all the Jasmine JavaScript test to JavaScript Test Driver because the jasmine-maven-plugin did not run test successfully on the command line.

--- my-calendar-gui/src/test/webapp/lib/js/jstd/CalendarAPITest.js
+++ my-calendar-gui/src/test/webapp/lib/js/jstd/CalendarAPITest.js
@@ -1,17 +1,286 @@
-TestCase("CalendarAPITestCase", {
-	getWeekDaysRemainingInWeekBefore20120215Test : function() {
+TestCase("CalendarAPITestCases", {
+	testGettingWeekDaysRemainingInWeekBeforeFeb15_2012 : function() {
 		var feb20120215 = new Date();
 		feb20120215.setFullYear(2012, 1, 15);
-		var monthTester = (new Tester()).getInstance(Month);
-		assertNotUndefined("monthTester should be defined.", monthTester);
-		var remainingWeekDays = monthTester._privates.getRemainingWeekDaysBefore(feb20120215);
-		assertNotUndefined("remainingWeekDays should be defined.", remainingWeekDays);
-		expect(remainingWeekDays.length).toEqual(3);
-		assertEquals(remainingWeekDays[0].getWeekDay(), "Sunday");
-		assertEquals(remainingWeekDays[0].getWeekDate(), 12);
-		assertEquals(remainingWeekDays[1].getWeekDay(), "Monday");
-		assertEquals(remainingWeekDays[1].getWeekDate(), 13);
-		assertEquals(remainingWeekDays[2].getWeekDay(), "Tuesday");
-		assertEquals(remainingWeekDays[2].getWeekDate(), 14);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("monthTester should be defined.", month);
+		var weekdays = month._privates.getRemainingWeekDaysBefore(feb20120215);
+		assertNotUndefined("weekdays should be defined.", weekdays);
+		assertEquals(3, weekdays.length);
+		assertEquals("Sunday", weekdays[0].getWeekDay());
+		assertEquals(12, weekdays[0].getWeekDate());
+		assertEquals("Monday", weekdays[1].getWeekDay());
+		assertEquals(13, weekdays[1].getWeekDate());
+		assertEquals("Tuesday", weekdays[2].getWeekDay());
+		assertEquals(14, weekdays[2].getWeekDate());
+	},
+	testGettingWeekDaysRemainingInWeekAfterFeb15_2012 : function(){
+		var feb20120215 = new Date();
+		feb20120215.setFullYear(2012, 1, 15);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var weekdays = month._privates.getRemainingWeekDaysAfter(feb20120215);
+		assertNotUndefined("weekdays should be defined.", weekdays);
+		assertEquals(7, weekdays.length);
+		assertEquals("Thursday", weekdays[4].getWeekDay());
+		assertEquals(16, weekdays[4].getWeekDate());
+		assertEquals("Friday", weekdays[5].getWeekDay());
+		assertEquals(17, weekdays[5].getWeekDate());
+		assertEquals("Saturday", weekdays[6].getWeekDay());
+		assertEquals(18, weekdays[6].getWeekDate());
+	},
+	testGettingWeekBeforeFeb19_2012 : function(){
+		var feb20120219 = new Date();
+		feb20120219.setFullYear(2012, 1, 19);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var week = month._privates.getWeekBefore(feb20120219);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals(7, week.weekdays.length);
+		assertEquals("Sunday", week.weekdays[0].getWeekDay());
+		assertEquals(12, week.weekdays[0].getWeekDate());
+		assertEquals("Monday", week.weekdays[1].getWeekDay());
+		assertEquals(13, week.weekdays[1].getWeekDate());
+		assertEquals("Tuesday", week.weekdays[2].getWeekDay());
+		assertEquals(14, week.weekdays[2].getWeekDate());
+		assertEquals("Wednesday", week.weekdays[3].getWeekDay());
+		assertEquals(15, week.weekdays[3].getWeekDate());
+		assertEquals("Thursday", week.weekdays[4].getWeekDay());
+		assertEquals(16, week.weekdays[4].getWeekDate());
+		assertEquals("Friday", week.weekdays[5].getWeekDay());
+		assertEquals(17, week.weekdays[5].getWeekDate());
+		assertEquals("Saturday", week.weekdays[6].getWeekDay());
+		assertEquals(18, week.weekdays[6].getWeekDate());
+	},
+	testGettingWeekAfterFeb19_2012 : function(){
+		var feb20120219 = new Date();
+		feb20120219.setFullYear(2012, 1, 19);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var week = month._privates.getWeekAfter(feb20120219);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals(7, week.weekdays.length);
+		assertEquals("Sunday", week.weekdays[0].getWeekDay());
+		assertEquals(26, week.weekdays[0].getWeekDate());
+		assertEquals("Monday", week.weekdays[1].getWeekDay());
+		assertEquals(27, week.weekdays[1].getWeekDate());
+		assertEquals("Tuesday", week.weekdays[2].getWeekDay());
+		assertEquals(28, week.weekdays[2].getWeekDate());
+		assertEquals("Wednesday", week.weekdays[3].getWeekDay());
+		assertEquals(29, week.weekdays[3].getWeekDate());
+		assertEquals("Thursday", week.weekdays[4].getWeekDay());
+		assertEquals(1, week.weekdays[4].getWeekDate());
+		assertEquals("Friday", week.weekdays[5].getWeekDay());
+		assertEquals(2, week.weekdays[5].getWeekDate());
+		assertEquals("Saturday", week.weekdays[6].getWeekDay());
+		assertEquals(3, week.weekdays[6].getWeekDate());
+	},
+	testGettingWeekDaysRemainingInWeekAfterFeb27_2012 : function(){
+		var feb20120227 = new Date();
+		feb20120227.setFullYear(2012, 1, 27);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var weekdays = month._privates.getRemainingWeekDaysAfter(feb20120227);
+		assertNotUndefined("weekdays should be defined.", weekdays);
+		assertEquals(7, weekdays.length);
+		assertEquals("Tuesday", weekdays[2].getWeekDay());
+		assertEquals(28, weekdays[2].getWeekDate());
+		assertEquals("Wednesday", weekdays[3].getWeekDay());
+		assertEquals(29, weekdays[3].getWeekDate());
+		assertEquals("Thursday", weekdays[4].getWeekDay());
+		assertEquals(1, weekdays[4].getWeekDate());
+		assertEquals("Friday", weekdays[5].getWeekDay());
+		assertEquals(2, weekdays[5].getWeekDate());
+		assertEquals("Saturday", weekdays[6].getWeekDay());
+		assertEquals(3, weekdays[6].getWeekDate());
+	},
+	testGettingWeekDaysRemainingInWeekBeforeFeb1_2012 : function(){
+		var feb20120201 = new Date();
+		feb20120201.setFullYear(2012, 1, 1);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var weekdays = month._privates.getRemainingWeekDaysBefore(feb20120201);
+		assertNotUndefined("weekdays should be defined.", weekdays);
+		assertEquals("Sunday", weekdays[0].getWeekDay());
+		assertEquals(29, weekdays[0].getWeekDate());
+		assertEquals("Monday", weekdays[1].getWeekDay());
+		assertEquals(30, weekdays[1].getWeekDate());
+		assertEquals("Tuesday", weekdays[2].getWeekDay());
+		assertEquals(31, weekdays[2].getWeekDate());
+	},
+	testGettingWeekBeforeFeb1_2012 : function(){
+		var feb20120201 = new Date();
+		feb20120201.setFullYear(2012, 1, 1);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var week = month._privates.getWeekBefore(feb20120201);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals(7, week.weekdays.length);
+		assertEquals("Sunday", week.weekdays[0].getWeekDay());
+		assertEquals(22, week.weekdays[0].getWeekDate());
+		assertEquals("Monday", week.weekdays[1].getWeekDay());
+		assertEquals(23, week.weekdays[1].getWeekDate());
+		assertEquals("Tuesday", week.weekdays[2].getWeekDay());
+		assertEquals(24, week.weekdays[2].getWeekDate());
+		assertEquals("Wednesday", week.weekdays[3].getWeekDay());
+		assertEquals(25, week.weekdays[3].getWeekDate());
+		assertEquals("Thursday", week.weekdays[4].getWeekDay());
+		assertEquals(26, week.weekdays[4].getWeekDate());
+		assertEquals("Friday", week.weekdays[5].getWeekDay());
+		assertEquals(27, week.weekdays[5].getWeekDate());
+		assertEquals("Saturday", week.weekdays[6].getWeekDay());
+		assertEquals(28, week.weekdays[6].getWeekDate());
+	},
+	testGettingWeekAfterFeb1_2012 : function(){
+		var feb20120201 = new Date();
+		feb20120201.setFullYear(2012, 1, 1);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var week = month._privates.getWeekAfter(feb20120201);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals(7, week.weekdays.length);
+		assertEquals("Sunday", week.weekdays[0].getWeekDay());
+		assertEquals(5, week.weekdays[0].getWeekDate());
+		assertEquals("Monday", week.weekdays[1].getWeekDay());
+		assertEquals(6, week.weekdays[1].getWeekDate());
+		assertEquals("Tuesday", week.weekdays[2].getWeekDay());
+		assertEquals(7, week.weekdays[2].getWeekDate());
+		assertEquals("Wednesday", week.weekdays[3].getWeekDay());
+		assertEquals(8, week.weekdays[3].getWeekDate());
+		assertEquals("Thursday", week.weekdays[4].getWeekDay());
+		assertEquals(9, week.weekdays[4].getWeekDate());
+		assertEquals("Friday", week.weekdays[5].getWeekDay());
+		assertEquals(10, week.weekdays[5].getWeekDate());
+		assertEquals("Saturday", week.weekdays[6].getWeekDay());
+		assertEquals(11, week.weekdays[6].getWeekDate());
+	},
+	testGettingWeekOfFeb1_2012: function(){
+		var feb20120201 = new Date();
+		feb20120201.setFullYear(2012, 1, 1);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var week = month._privates.getWeek(feb20120201);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals(7, week.weekdays.length);
+		assertEquals("Sunday", week.sunday.getWeekDay());
+		assertEquals(29, week.sunday.getWeekDate());
+		assertEquals("Monday", week.monday.getWeekDay());
+		assertEquals(30, week.monday.getWeekDate());
+		assertEquals("Tuesday", week.tuesday.getWeekDay());
+		assertEquals(31, week.tuesday.getWeekDate());
+		assertEquals("Wednesday", week.wednesday.getWeekDay());
+		assertEquals(1, week.wednesday.getWeekDate());
+		assertEquals("Thursday", week.thursday.getWeekDay());
+		assertEquals(2, week.thursday.getWeekDate());
+		assertEquals("Friday", week.friday.getWeekDay());
+		assertEquals(3, week.friday.getWeekDate());
+		assertEquals("Saturday", week.saturday.getWeekDay());
+		assertEquals(4, week.saturday.getWeekDate());
+	},
+	testGettingWeekOfFeb29_2012 : function(){
+		var feb20120229 = new Date();
+		feb20120229.setFullYear(2012, 1, 29);
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		var week = month._privates.getWeek(feb20120229);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals(7, week.weekdays.length);
+		assertEquals("Sunday", week.sunday.getWeekDay());
+		assertEquals(26, week.sunday.getWeekDate());
+		assertEquals("Monday", week.monday.getWeekDay());
+		assertEquals(27, week.monday.getWeekDate());
+		assertEquals("Tuesday", week.tuesday.getWeekDay());
+		assertEquals(28, week.tuesday.getWeekDate());
+		assertEquals("Wednesday", week.wednesday.getWeekDay());
+		assertEquals(29, week.wednesday.getWeekDate());
+		assertEquals("Thursday", week.thursday.getWeekDay());
+		assertEquals(1, week.thursday.getWeekDate());
+		assertEquals("Friday", week.friday.getWeekDay());
+		assertEquals(2, week.friday.getWeekDate());
+		assertEquals("Saturday", week.saturday.getWeekDay());
+		assertEquals(3, week.saturday.getWeekDate());
+	},
+	testGettingPreviousMonthsTotalDaysWhenCurrentMonthFeb2012: function(){
+		var feb20120201 = new Date();
+		feb20120201.setFullYear(2012, 1, 1);
+		
+		assertEquals(1, feb20120201.getMonth());
+		var maxDay = (new Month()).getPreviousMonthTotalDays(feb20120201);
+		assertNotUndefined("MaxDay should be defined.", maxDay);
+		assertEquals(31, maxDay);
+	},
+	testGettingTotalDaysInFeb2012: function(){
+		var month = new Month();
+		assertNotUndefined("month should be defined.", month);
+		var maxDay = (new Month()).monthNames[1].getTotalDays(2012);
+		assertNotUndefined("MaxDay should be defined.", maxDay);
+		assertEquals(29, maxDay);
+	},
+	testGettingPreviousMonthsTotalDaysWhenCurrentMonthIsMarch2012: function(){
+		var mar20120301 = new Date();
+		mar20120301.setFullYear(2012, 2, 1);
+		assertEquals(2, mar20120301.getMonth());
+		var maxDay = (new Month()).getPreviousMonthTotalDays(mar20120301);
+		assertNotUndefined("MaxDay should be defined.", maxDay);
+		assertEquals(29, maxDay);
+	},
+	testThatWhenSubtractingOneDayFromJan1_2012ResultsInDec31_2011: function(){
+		var date = new Date();
+		date.setFullYear(2012, 0, 1);
+		date.setDate(date.getDate()-1);
+		assertEquals(11, date.getMonth());
+		assertEquals(31, date.getDate());
+		
+		assertEquals(2011, (date.getFullYear()));
+	},
+	testThatFirstWeekInJan2012IsFromSun1stToSat7th: function(){
+		var date = new Date();
+		date.setFullYear(2012, 0, 7);
+		
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		
+		var week =  month._privates.getWeek(date);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals("Sunday", week.sunday.getWeekDay());
+		assertEquals(1, week.sunday.getWeekDate());
+		assertEquals("Monday", week.monday.getWeekDay());
+		assertEquals(2, week.monday.getWeekDate());
+		assertEquals("Tuesday", week.tuesday.getWeekDay());
+		assertEquals(3, week.tuesday.getWeekDate());
+		assertEquals("Wednesday", week.wednesday.getWeekDay());
+		assertEquals(4, week.wednesday.getWeekDate());
+		assertEquals("Thursday", week.thursday.getWeekDay());
+		assertEquals(5, week.thursday.getWeekDate());
+		assertEquals("Friday", week.friday.getWeekDay());
+		assertEquals(6, week.friday.getWeekDate());
+		assertEquals("Saturday", week.saturday.getWeekDay());
+		assertEquals(7, week.saturday.getWeekDate());
+	},
+	testThatLastWeekInJan2012IsFromSun29thToSat4th: function(){
+		var date = new Date();
+		date.setFullYear(2012, 1, 3);
+		
+		var month = (new Tester()).getInstance(Month);
+		assertNotUndefined("month should be defined.", month);
+		
+		var week = month._privates.getWeek(date);
+		assertNotUndefined("week should be defined.", week);
+		assertEquals("Sunday", week.sunday.getWeekDay());
+		assertEquals(29, week.sunday.getWeekDate());
+		assertEquals("Monday", week.monday.getWeekDay());
+		assertEquals(30, week.monday.getWeekDate());
+		assertEquals("Tuesday", week.tuesday.getWeekDay());
+		assertEquals(31, week.tuesday.getWeekDate());
+		assertEquals("Wednesday", week.wednesday.getWeekDay());
+		assertEquals(1, week.wednesday.getWeekDate());
+		assertEquals("Thursday", week.thursday.getWeekDay());
+		assertEquals(2, week.thursday.getWeekDate());
+		assertEquals("Friday", week.friday.getWeekDay());
+		assertEquals(3, week.friday.getWeekDate());
+		assertEquals("Saturday", week.saturday.getWeekDay());
+		assertEquals(4, week.saturday.getWeekDate());
 	}
+	
+	
 });

30ed80695f98c25c1b442b4c87256d355d453eae
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-01-11 17:08:04 -0600

    Re-factored for continuous integration support.

--- src/test/scripts/test.sh
+++ src/test/scripts/test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 BASE_DIR=`dirname $0`
-
-echo ""
+#prepare
+command="testacular start $BASE_DIR/../resources/$*"
+echo "PATH='$PATH'"
+echo "BASE_DIR='$BASE_DIR'"
+echo "Input Parameter 0='$0'"
+echo "Input Parameter 1='$1'"
+echo "Input Parameter *='$*'"
 echo "Starting Testacular Server (http://vojtajina.github.com/testacular)"
+echo "Executing Command = '$command'"
 echo "-------------------------------------------------------------------"
-
-testacular start $BASE_DIR/../resources/testacular.ci.conf.js $*
+#execute
+$command

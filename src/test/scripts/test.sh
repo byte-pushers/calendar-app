cbec8c8cc0c151cbc566656027586582421e41ad
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-02-12 23:47:17 -0600

    Re-factored to pass system path variable to ant task.

--- src/test/scripts/test.sh
+++ src/test/scripts/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+export PATH=$PATH:$1
 BASE_DIR=`dirname $0`
 #prepare
-command="testacular start $BASE_DIR/../resources/$*"
+command="testacular start $BASE_DIR/../resources/$2"
 echo "PATH='$PATH'"
 echo "BASE_DIR='$BASE_DIR'"
 echo "Input Parameter 0='$0'"
 echo "Input Parameter 1='$1'"
+echo "Input Parameter 2='$2'"
 echo "Input Parameter *='$*'"
 echo "Starting Testacular Server (http://vojtajina.github.com/testacular)"
 echo "Executing Command = '$command'"

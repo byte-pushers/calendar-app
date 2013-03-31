38ba7fda4d2347b9ed7f51950549c2c1ce1635f0
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-03-31 11:31:21 -0500

    Create AngularJS seed default project structure for cloudbees CI.

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -1,12 +1,15 @@
 basePath = '../';
-
+//'app/js/**/*.js',
 files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/lib/angular/angular.js',
   'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
+  'app/js/controllers.js',
+  'app/js/directives.js',
+  'app/js/filters.js',
+  'app/js/services.js',
   'test/unit/**/*.js'
 ];
 

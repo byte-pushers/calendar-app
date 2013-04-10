9d7b699d7ae7cdc01dd2249cab35f21393cdaaf7
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-04-10 14:36:13 -0500

    Re-factor code to work with IE 9

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -64,32 +64,6 @@ junitReporter = {
   outputFile: 'test_out/unit.xml',
   suite: 'unit'
 };
-
-// web server port
-// CLI --port 9876
-port = 9876;
-
-// cli runner port
-// CLI --runner-port 9100
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-// CLI --colors --no-colors
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-// CLI --log-level debug
-logLevel = LOG_INFO;
-
-// If browser does not capture in given timeout [ms], kill it
-// CLI --capture-timeout 5000
-captureTimeout = 10000;
-
 // Auto run tests on start (when browsers are captured) and exit
 // CLI --single-run --no-single-run
 singleRun = true;
-
-// report which specs are slower than 500ms
-// CLI --report-slower-than 500
-reportSlowerThan = 500;

10bd43fa8c2eff9d718211c3420db6ceae0a0c42
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-04-10 15:22:50 -0500

    Re-named to troubleshoot CI problems.

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -4,7 +4,10 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
+  'app/lib/angular/angular-cookies.js',
+  'app/lib/angular/angular-loader.js',
+  'app/lib/angular/angular-resource.js',
+  'app/lib/angular/angular-sanitize.js',
   'app/lib/jquery/jquery-1.7.2.js',
   'app/js/com.noesiscode.js',
   'app/js/com.noesiscode.extentions.Console.js',
@@ -55,7 +58,7 @@ autoWatch = false;
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-browsers = ['Chrome', 'Firefox'];
+browsers = ['Chrome'];
 // use dots reporter, as travis terminal does not support escaping sequences
 // possible values: 'dots', 'progress', 'junit'
 // CLI --reporters progress
@@ -64,6 +67,37 @@ junitReporter = {
   outputFile: 'test_out/unit.xml',
   suite: 'unit'
 };
+
+// web server port
+// CLI --port 9876
+port = 9876;
+
+// cli runner port
+// CLI --runner-port 9100
+runnerPort = 9100;
+
+// enable / disable colors in the output (reporters and logs)
+// CLI --colors --no-colors
+colors = true;
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+// CLI --log-level debug
+logLevel = LOG_INFO;
+
+// If browser does not capture in given timeout [ms], kill it
+// CLI --capture-timeout 5000
+captureTimeout = 9000;
+
 // Auto run tests on start (when browsers are captured) and exit
 // CLI --single-run --no-single-run
 singleRun = true;
+
+// report which specs are slower than 500ms
+// CLI --report-slower-than 500
+reportSlowerThan = 500;
+
+// compile coffee scripts
+preprocessors = {
+    '**/*.coffee': 'coffee'
+};

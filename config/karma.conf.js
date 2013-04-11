104a84ff2ca1498a2de94b4b8625f05cf5ee5adf
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-04-10 19:11:59 -0500

    Switch from Testactular to Karma

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -42,62 +42,12 @@ files = [
     'test/unit/jasmine/CalendarSpec.js'
 ];
 
-// list of files to exclude
-exclude = ['app/lib/angular/angular-*.min.js'];
+autoWatch = true;
 
-// enable / disable watching file and executing tests whenever any file changes
-// CLI --auto-watch --no-auto-watch
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-// CLI --browsers Chrome,Firefox,Safari
 browsers = ['Chrome'];
-// use dots reporter, as travis terminal does not support escaping sequences
-// possible values: 'dots', 'progress', 'junit'
-// CLI --reporters progress
-reporters = ['dots','junit'];
+
 junitReporter = {
-    outputFile: 'test_out/unit.xml',
-    suite: 'unit'
+  outputFile: 'test_out/unit.xml',
+  suite: 'unit'
 };
 
-// web server port
-// CLI --port 9876
-port = 9876;
-
-// cli runner port
-// CLI --runner-port 9100
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-// CLI --colors --no-colors
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-// CLI --log-level debug
-logLevel = LOG_INFO;
-
-// If browser does not capture in given timeout [ms], kill it
-// CLI --capture-timeout 5000
-captureTimeout = 9000;
-
-// Auto run tests on start (when browsers are captured) and exit
-// CLI --single-run --no-single-run
-singleRun = true;
-
-// report which specs are slower than 500ms
-// CLI --report-slower-than 500
-reportSlowerThan = 500;
-
-// compile coffee scripts
-preprocessors = {
-    '**/*.coffee': 'coffee'
-};

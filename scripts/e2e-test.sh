7bbfcf53c6495d0cc36434408448392394d4b145
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-04-18 13:20:54 -0500

    Moving away from AnguarJS seed project structure to Maven project structure.

--- scripts/e2e-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=`dirname $0`
-
-echo ""
-echo "Starting karma Server (http://vojtajina.github.com/karma)"
-echo "-------------------------------------------------------------------"
-
-karma start $BASE_DIR/../config/karma-e2e.conf.js $*

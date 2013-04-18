2a68e3ef3f4516556e45e72464b0690a877f4b07
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-04-18 16:49:43 -0500

    Moving away from AnguarJS seed project structure to Maven project structure.

--- src/test/scripts/cloudbees.ci.unit.test.sh
+++ src/test/scripts/cloudbees.ci.unit.test.sh
@@ -1,34 +1,9 @@
-export DISPLAY=:1
-Xvfb :1 &
+#!/bin/bash
 
-#
-# Fetch node and testacular if we don't have it already
-#
+BASE_DIR=`dirname $0`
 
-node_version=v0.8.14
-install_name=node-$node_version-linux-x64
-node_home=$PWD/$install_name
+echo ""
+echo "Starting karma Server (http://vojtajina.github.com/karma)"
+echo "-------------------------------------------------------------------"
 
-#if [ ! -e $install_name.tar.gz ]
-#then
-    wget http://nodejs.org/dist/$node_version/$install_name.tar.gz
-    tar xf $install_name.tar.gz
-    $node_home/bin/npm install -g karma
-#fi
-
-#
-# run the Angular.js tests (using a browser on the build server)
-#
-
-export PATH=$PATH:$node_home/bin
-
-src/test/scripts/cloudbees.ci.unit.test.sh  #--single-run --browsers="Chrome,Firefox" --reporters="dots,junit" --no-colors
-
-#
-# skipping the Angular.js e2e tests (this requires a server too)
-#
-
-#node scripts/web-server.js > /dev/null &
-#NODE_PID=$!
-#src/test/scripts/e2e-test.sh --single-run --browsers="Chrome,Firefox" --reporters="dots,junit" --no-colors
-#kill -s TERM $NODE_PID
+karma start $BASE_DIR/../resources/karma.ci.conf.js $*

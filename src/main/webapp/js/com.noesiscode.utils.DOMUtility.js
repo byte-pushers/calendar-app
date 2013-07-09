4adc7ed781ae0608697b5262e9e216c7b90cc279
Tonté Pouncil <pouncilt.developer@gmail.com>
2013-07-09 18:45:20 -0500

    IE Testing via VMWare Fusion

--- src/main/webapp/js/com.noesiscode.utils.DOMUtility.js
+++ src/main/webapp/js/com.noesiscode.utils.DOMUtility.js
@@ -52,13 +52,17 @@ if (typeof document.querySelector !== 'function') {
         "use strict";
         return document.querySelector(selector);
     };
-} else if (typeof document.getElementsByClassName !== 'function') {
+}else {
+    throw ("document.querySelector() method is not supported by your browser.  Please contact the administrator for this app.");
+}
+
+if (typeof document.getElementsByClassName !== 'function') {
     NoesisCode.DOMUtility.querySelector = function (selector) {
         "use strict";
         return document.getElementsByClassName(selector);
     };
 } else {
-    throw ("document.querySelector() method is not supported by your browser.  Please contact the administrator for this app.");
+    throw ("document.getElementsByClassName() method is not supported by your browser.  Please contact the administrator for this app.");
 }
 
 if (typeof document.querySelectorAll === 'function') {
